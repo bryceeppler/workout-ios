a0bbd186f847bdcf8d2a0979ffa42df840a73e7c
Bryce Eppler <eppler97@gmail.com>
2023-02-12 06:31:10 -0800

    Initial Commit

---
+++ Onslaught/OnslaughtApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnslaughtApp.swift
+//  Onslaught
+//
+//  Created by Bryce Eppler on 2023-02-12.
+//
+
+import SwiftUI
+
+@main
+struct OnslaughtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
